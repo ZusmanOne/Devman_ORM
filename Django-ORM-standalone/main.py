16642711d57198457f86ba00698574f867503ab0
Nikita <bigmama93@mail.ru>
2022-04-03 10:07:30 +0300

    step 11 finish

--- Django-ORM-standalone/main.py
+++ Django-ORM-standalone/main.py
@@ -1,19 +1,25 @@
 import os
-
+from django.utils.timezone import localtime
 import django
-
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'settings')
 django.setup()
-
-from datacenter.models import Passcard  # noqa: E402
+from datacenter.models import Passcard, Visit  # noqa: E402
 
 if __name__ == '__main__':
     # Программируем здесь
-    # client = Passcard.objects.get(id=1)
-    # active_client = []
-    # for client in Passcard.objects.all():
-    #     if client.is_active:
-    #         active_client.append(client)
+    client = Passcard.objects.get(id=1)
+    active_client = []
+    for client in Passcard.objects.all():
+        if client.is_active:
+            active_client.append(client)
     print("Всего пропусков:", Passcard.objects.count())
     print("Активных пропусков:", len(Passcard.objects.filter(is_active=True)))
-    #print('Количество пропусков:', Passcard.objects.count())  # noqa: T001
+    print(Visit.objects.filter(leaved_at=None))
+    entered = Visit.objects.filter(leaved_at=None)
+    for enter in entered:
+        print("Зашёл в хранилище, время по Москве:", localtime(enter.entered_at))
+        date = localtime() - localtime(enter.entered_at)
+        print('Находится в хранилище:', date, sep='\n')
+    visit_name = Visit.objects.filter(leaved_at=None)
+    for visit in visit_name:
+        print(visit.passcard)
