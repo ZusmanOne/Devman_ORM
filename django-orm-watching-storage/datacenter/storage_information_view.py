1256c9024e0e0081e3042ad7795c66f9f0275e9f
Nikita <bigmama93@mail.ru>
2022-04-13 21:30:16 +0300

    edit style coding

--- django-orm-watching-storage/datacenter/storage_information_view.py
+++ django-orm-watching-storage/datacenter/storage_information_view.py
@@ -2,8 +2,8 @@ from datacenter.models import Visit
 from django.shortcuts import render
 from django.utils.timezone import localtime
 
-seconds_in_hour = 3600
-minutes_in_hour = 60
+SECONDS_IN_HOUR = 3600
+MINUTES_IN_HOUR = 60
 
 
 def get_duration(visit):
@@ -17,21 +17,21 @@ def get_duration(visit):
 
 
 def format_duration(duration):
-    duration_hours = int(duration) // seconds_in_hour
-    duration_minutes = int(duration % seconds_in_hour) // minutes_in_hour
+    duration_hours = int(duration) // SECONDS_IN_HOUR
+    duration_minutes = int(duration % SECONDS_IN_HOUR) // MINUTES_IN_HOUR
     return "%sч : %sм " % (duration_hours, duration_minutes)
 
 
 def storage_information_view(request):
     not_outs = Visit.objects.filter(leaved_at=None)
-    non_closed_visits = []
+    serialized_visits = []
     for i in not_outs:
-        non_closed_visits.append({'who_entered': i.passcard,
+        serialized_visits.append({'who_entered': i.passcard,
                                   'entered_at': i.entered_at,
                                   'duration': format_duration(get_duration(i)),
                                   }),
 
     context = {
-        'non_closed_visits': non_closed_visits,
+        'non_closed_visits': serialized_visits,
     }
     return render(request, 'storage_information.html', context,)
