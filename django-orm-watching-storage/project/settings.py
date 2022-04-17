8562898ce891932bc89b9f194ff0fc4ac74fca4e
Nikita <bigmama93@mail.ru>
2022-04-17 14:16:04 +0300

    lesson_2 step_10

--- django-orm-watching-storage/project/settings.py
+++ django-orm-watching-storage/project/settings.py
@@ -1,22 +1,27 @@
 import os
+from environs import Env
+
+env = Env()
+env.read_env()
+
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
-        'PORT': '5434',
-        'NAME': 'checkpoint',
-        'USER': 'guard',
-        'PASSWORD': 'osim5',
+        'HOST': env('DATABASE_HOST'),
+        'PORT': env('DATABASE_PORT'),
+        'NAME': env('DATABASE_NAME'),
+        'USER': env('DATABASE_USER'),
+        'PASSWORD': env('DATABASE_PASSWORD'),
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = env('SECRET_KEY')
 
-DEBUG = True
-
+DEBUG = (os.environ.get('DEBUG') == "True")
+print(env('DEBUG'))
 ROOT_URLCONF = 'project.urls'
 
 ALLOWED_HOSTS = ['*']
